0fcecb91bbcaefa8a0f840c3ba86653828607117
kawatakutaku <kawataku.89love@gmail.com>
2022-01-03 15:07:29 +0900

    spotからtrip_nameにカラム名を変更した

--- backend/database/seeders/TripsTableSeeder.php
+++ backend/database/seeders/TripsTableSeeder.php
@@ -17,13 +17,13 @@ class TripsTableSeeder extends Seeder
     public function run()
     {
         // TODO factoryを使って、自動的にデータを生成する方法を採用する
-        $spots = ['家', '空港', '港'];
+        $trip_names = ['家', '空港', '港'];
         $start_days = ['2021-12-31', '2022-1-1', '2022-1-2'];
         $end_days = ['2021-12-31', '2022-1-1', '2022-1-2'];
 
         for ($i=0; $i<3; $i++) {
             DB::table('trips')->insert([
-                'spot' => $spots[$i],
+                'spot' => $trip_names[$i],
                 'start_day' => $start_days[$i],
                 'end_day' => $end_days[$i],
                 'created_at' => Carbon::now(),
