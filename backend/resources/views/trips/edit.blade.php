b20fa7cda0d49592d75b2c28665b14590afd0180
kawatakutaku <kawataku.89love@gmail.com>
2022-01-05 21:14:18 +0900

    編集機能を少し変更した

--- backend/resources/views/trips/edit.blade.php
+++ backend/resources/views/trips/edit.blade.php
@@ -4,22 +4,22 @@
 <div class="container-fluid mt-5">
     <div class="row justify-content-center d-flex">
         <div class="col-md-8 col-xl-6">
-            <!-- TODO editやshowのtrip_idのキーをtrip_idに変更する-->
             <form action="{{ route('trips.update', ['trip' => '$trip_id']) }}" method="post">
                 @csrf
                 <div class="row p-2 justify-content-center">
                     <div class="col-6">
-                        <input type="text" class="form-control" name="trip_name" id="trip_name" placeholder="{{ $trip->trip_name }}">
+                        <!-- TODO valueを条件分岐して、最初は初期値を表示するようにする -->
+                        <input type="text" class="form-control" name="trip_name" id="trip_name" value="{{ old('trip_name') ?? $trip->trip_name }} ">
                     </div>
                 </div>
                 <div class="row p-2 mt-3 justify-content-center">
                     <div class="col-6" id="start">
-                        <input type="text" class="form-control" name="start_day" id="start_day" placeholder="{{ $trip->start_day }}">
+                        <input type="text" class="form-control" name="start_day" id="start_day" value="{{ old('start_day') ?? $trip->start_day }}">
                     </div>
                 </div>
                 <div class="row p-2 mt-3 justify-content-center">
                     <div class="col-6" id="end">
-                        <input type="text" class="form-control" name="end_day" id="end_day" placeholder="{{ $trip->end_day }}">
+                        <input type="text" class="form-control" name="end_day" id="end_day" value="{{ old('end_day') ?? $trip->end_day }}">
                     </div>
                 </div>
                 <div class="col-6 col-md-4 col-xl-3 col-xs-12 mx-auto mt-5">
