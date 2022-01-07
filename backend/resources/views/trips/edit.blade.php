88b031b9e461f1fadd7c1165e9fc82caaf602565
kawatakutaku <kawataku.89love@gmail.com>
2022-01-07 21:16:26 +0900

    storeとupdateのform requestを作成したのと、edit画面にて、エラーメッセージを表示するようにした

--- backend/resources/views/trips/edit.blade.php
+++ backend/resources/views/trips/edit.blade.php
@@ -11,6 +11,9 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <!-- TODO 以下を、bootstrapを使って記述をしているからtailwindに合わせる-->
+                    @if ($errors->any())
+                        <x-alert />
+                    @endif
                     <form action="{{ route('trips.update', ['trip' => $trip->id]) }}" method="post">
                         @method('put')
                         @csrf
