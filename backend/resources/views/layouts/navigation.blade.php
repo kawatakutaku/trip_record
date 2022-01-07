13908e5709d222e0b8bd2775682024c5fc6ec3b2
kawatakutaku <kawataku.89love@gmail.com>
2022-01-07 17:08:57 +0900

    ログイン後のリダイレクト先をmypageに変更して、dashboardのリンクを削除した

--- backend/resources/views/layouts/navigation.blade.php
+++ backend/resources/views/layouts/navigation.blade.php
@@ -11,11 +11,6 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
 
                 <!-- MyPageへのリンク-->
                 <!-- TODO Dashboardのように言語ファイルを使用する-->
