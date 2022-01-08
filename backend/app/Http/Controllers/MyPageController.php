e76102961c24fbb5a92d4542d41be2344cb35116
kawatakutaku <kawataku.89love@gmail.com>
2022-01-08 13:35:48 +0900

    ユーザーのidを元にしてデータを取得するように変更した

--- backend/app/Http/Controllers/MyPageController.php
+++ backend/app/Http/Controllers/MyPageController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Trip;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MyPageController extends Controller
 {
     public function __invoke()
     {
-        // 全部の旅行情報を取得している
-        $trips = Trip::all();
+        // TODO 複数のユーザーがログインしている場合は全員のユーザーのidを取得してしまうのではないか？
+        // 指定のuser_idのユーザーを取得する
+        $userId = Auth::id();
+        $trips = Trip::where('user_id', $userId)->get();
 
         return view('mypage', [
             'trips' => $trips,
