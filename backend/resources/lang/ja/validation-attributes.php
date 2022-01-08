0a5f1d021d1cb493cd7a047da7f1a4848e5552f2
kawatakutaku <kawataku.89love@gmail.com>
2022-01-08 13:08:21 +0900

    validation-attributes内に記述しても日本語表記ができなかったが、FormRequest内に書いたら上手くいった

--- backend/resources/lang/ja/validation-attributes.php
+++ backend/resources/lang/ja/validation-attributes.php
@@ -1,6 +1,5 @@
 <?php
 
-// TODO attributes内を変更したのに、エラーメッセージが日本語にならない
 return [
     'attributes' => [
         'address'               => 'Address',
@@ -37,8 +36,5 @@ return [
         'title'                 => 'Title',
         'username'              => 'Username',
         'year'                  => 'Year',
-        'trip_name'             => '旅行タイトル',
-        'start_day'             => '出発日',
-        'end_day'             => '帰宅日',
     ],
 ];
