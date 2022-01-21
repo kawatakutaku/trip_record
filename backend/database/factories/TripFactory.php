c64c3f3cd7852dd6cf89873520f6e877d7d31c9f
kawazu <kawazu@spookies.co.jp>
2022-01-21 13:18:07 +0900

    factoryを作成した

--- backend/database/factories/TripFactory.php
+++ backend/database/factories/TripFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class BookFactory extends Factory
+class TripFactory extends Factory
 {
     /**
      * @return array
@@ -12,9 +12,8 @@ class BookFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => $this->faker->sentence(),
-            'description' => $this->faker->realText(),
-            'book_img' => $this->faker->image(),
+            'message' => $this->faker->realText(),
+            'img' => $this->faker->image(),
             'created_at' => $this->faker->dateTimeThisDecade(),
             'updated_at' => $this->faker->dateTimeThisDecade(),
         ];
