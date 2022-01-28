1687273ef538494463ac5d26f274111611e3c391
kawatakutaku <kawataku.89love@gmail.com>
2022-01-28 21:45:38 +0900

    CU-24wgdkg userのfactoryを追記した

--- backend/database/factories/Users/UserFactory.php
+++ backend/database/factories/Users/UserFactory.php
@@ -18,6 +18,11 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'register_token' => Str::random(10),
+            'password_reset_token' => Str::random(10),
+            'sex' => $this->faker->randomElement(['男性', '女性']),
+            'img' => $this->faker->image(),
+            'profile' => $this->faker->realText(30),
             'created_at' => $this->faker->dateTimeThisDecade(),
             'updated_at' => $this->faker->dateTimeThisDecade(),
         ];
