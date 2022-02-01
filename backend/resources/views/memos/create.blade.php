3e7c3efb65744a94d154da2170b7c4927bd27ad6
kawatakutaku <kawataku.89love@gmail.com>
2022-02-01 21:44:45 +0900

    CU-25dpq8w templateを編集した

--- backend/resources/views/memos/create.blade.php
+++ backend/resources/views/memos/create.blade.php
@@ -1,8 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            <!-- TODO Dashboardのように言語ファイルを使用する-->
-            本の投稿
+            <!-- TODO: Dashboardのように言語ファイルを使用する-->
+            メモの投稿
         </h2>
     </x-slot>
 
@@ -10,27 +10,20 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <!-- TODO エラーメッセージをそれぞれのフォームの下に表示するにする -->
+                    <!-- TODO: エラーメッセージをそれぞれのフォームの下に表示するにする -->
                     @if ($errors->any())
                         <x-alert />
                     @endif
-                    <form action="{{ route('books.store') }}" method="post">
+                    <form action="{{ route('memos.store') }}" method="post">
                         @csrf
                         <div class="row p-2 justify-content-center">
                             <div class="col-6">
-                                <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="title" id="title" value="{{ old('title') }}" placeholder="タイトル">
+                                <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="memo" id="memo" value="{{ old('memo') }}" placeholder="メモ">
                             </div>
                         </div>
                         <div class="row p-2 mt-3 justify-content-center">
-                            <div class="col-6" id="start">
-                                <!-- TODO カレンダーを使った入力方法を採用する-->
-                                <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="description" id="description" value="{{ old('description') }}" placeholder="説明">
-                            </div>
-                        </div>
-                        <div class="row p-2 mt-3 justify-content-center">
-                            <div class="col-6" id="end">
-                                <!-- TODO カレンダーを使った入力方法を採用する-->
-                                <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="book_img" id="book_img" value="{{ old('book_img') }}" placeholder="画像">
+                            <div class="col-6" id="img">
+                                <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="img" id="img" value="{{ old('img') }}" placeholder="画像">
                             </div>
                         </div>
                         <div class="col-6 col-md-4 col-xl-3 col-xs-12 mx-auto mt-5">
---
+++ backend/resources/views/memos/index.blade.php
@@ -0,0 +1,33 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <!-- TODO: Dashboardのように言語ファイルを使用する-->
+            メモの一覧
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <!-- TODO: エラーメッセージをそれぞれのフォームの下に表示するにする -->
+                    @foreach ($memos as $memo)
+                    <div class="row p-2 justify-content-center">
+                        <div class="col-6">
+                            <p class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="memo" id="memo">{{ $memo->memo }}</p>
+                        </div>
+                    </div>
+                    <div class="row p-2 mt-3 justify-content-center">
+                        <div class="col-6" id="img">
+                            <p class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="img" id="img">{{ $memo->img }}</p>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4 col-xl-3 col-xs-12 mx-auto mt-5">
+                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-gray-700 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button"> 次へ</button>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
