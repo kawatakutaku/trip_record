f7eb89abd92bce53dd2d337fde63c5e810372c24
kawatakutaku <kawataku.89love@gmail.com>
2022-02-02 09:51:40 +0900

    CU-25dpq8w memoを表示するように記述した

--- backend/resources/views/memos/index.blade.php
+++ backend/resources/views/memos/index.blade.php
@@ -10,8 +10,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <!-- TODO: エラーメッセージをそれぞれのフォームの下に表示するにする -->
-                    @foreach ($memos as $memo)
+                    @foreach($memos as $memo)
                     <div class="row p-2 justify-content-center">
                         <div class="col-6">
                             <p class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="memo" id="memo">{{ $memo->memo }}</p>
