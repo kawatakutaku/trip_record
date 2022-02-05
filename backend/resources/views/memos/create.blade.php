dacfb72f5e1197b7028bdfe960b45284c58bcfb1
kawatakutaku <kawataku.89love@gmail.com>
2022-02-05 20:03:55 +0900

    CU-25dpq8w templateをブラッシュアップした

--- backend/resources/views/memos/create.blade.php
+++ backend/resources/views/memos/create.blade.php
@@ -18,7 +18,7 @@
                         @csrf
                         <div class="row p-2 justify-content-center">
                             <div class="col-6">
-                                <textarea type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="memo" id="memo" value="{{ old('memo') }}" placeholder="メモ"></textarea>
+                                <x-textarea name="memo" id="memo" value="{{ old('memo') }}" placeholder="メモ" />
                             </div>
                         </div>
                         <div class="row p-2 mt-3 justify-content-center">
@@ -34,120 +34,4 @@
             </div>
         </div>
     </div>
-
-    <script>
-        // TODO trip用のjsからbook用のjsに変更する必要がある
-        $(function(){
-            $.datetimepicker.setLocale('ja');
-            $('.bigDate').datetimepicker({
-                timepicker: false,
-                format: 'Y-m-d',
-                numberOfMonths: 2
-            });
-            let start_time, end_time;
-            let now = new Date();
-            // 今日の日付-1日しておくことでifが上手くいく
-            now.setDate(now.getDate() - 1);
-            console.log('now:', now);
-            $('#start').find(".bigDate").on("blur", function () {
-                let error = false;
-                let value = $(this).val();
-                // 今日の日付を取得する
-                // フォームに入力された年月日のを入れる
-                let year_ls = []
-                let month_ls = []
-                let day_ls = []
-
-                // フォームに入力されたのはstringだから数字に直す
-                for (let i=0; i<10; i++){
-                    if (i < 4){
-                        year_ls += value[i];
-                    }else if(4 < i && i < 7){
-                        month_ls += value[i];
-                    }else if(7 < i){
-                        day_ls += value[i];
-                    }
-                }
-                // Dateの月は0~11になっている、日付は当日までをOKにしておくため
-                start_time = new Date(Number(year_ls), Number(month_ls)-1, Number(day_ls));
-    
-                error = start_time.getTime() <= now;
-    
-                if (error) {
-                    //エラーで、エラーメッセージがなかったら
-                    if(!$(this).nextAll('span.error-info').length) {
-                        //メッセージを後ろに追加
-                        $(this).after('<span class = "error-info text-danger">' + now.getFullYear() + '年' + Number(now.getMonth() + 1) + '月' + Number(now.getDate() + 1) + '日' + '以降の日付を入力してください'+ '</span>');
-                    }
-                }else{
-                    //エラーじゃないのにメッセージがあったら
-                    if($(this).nextAll('span.error-info').length) {
-                        //消す
-                        $(this).nextAll('span.error-info').remove();
-                    }
-                }
-    
-                $('.bigDate').datetimepicker({
-                    defaultDate: start_time,
-                    timepicker: false,
-                    format: 'Y-m-d',
-                    numberOfMonths: 2
-                });
-            });
-            $('#end').find(".bigDate").on("blur", function () {
-                let error = false;
-                let value = $(this).val();
-                // 今日の日付を取得する
-                // フォームに入力された年月日のを入れる
-                let year_ls = []
-                let month_ls = []
-                let day_ls = []
-    
-                // フォームに入力されたのはstringだから数字に直す
-                for (let i=0; i<10; i++){
-                    if (i < 4){
-                        year_ls += value[i];
-                    }else if(4 < i && i < 7){
-                        month_ls += value[i];
-                    }else if(7 < i){
-                        day_ls += value[i];
-                    }
-                }
-                // Dateの月は0~11になっている、日付は当日までをOKにしておくため
-                end_time = new Date(Number(year_ls), Number(month_ls)-1, Number(day_ls)+1);
-                console.log('end:', end);
-    
-                if (end_time.getTime() <= start_time.getTime()){
-                    error = true;
-                }
-    
-                if (error) {
-                    //エラーで、エラーメッセージがなかったら
-                    if(!$(this).nextAll('span.error-info').length) {
-                        //メッセージを後ろに追加
-                        $(this).after('<span class = "error-info text-danger">' + start_time.getFullYear() + '年' + Number(start_time.getMonth() + 1) + '月' + start_time.getDate() + '日以降の日付を入力してください'+ '</span>');
-                    }
-                }else{
-                    //エラーじゃないのにメッセージがあったら
-                    if($(this).nextAll('span.error-info').length) {
-                        //消す
-                        $(this).nextAll('span.error-info').remove();
-                    }
-                }
-    
-                $('.bigDate').datetimepicker({
-                    defaultDate: end_time,
-                    timepicker: false,
-                    format: 'Y-m-d',
-                    numberOfMonths: 2
-                });
-            });
-            $('form').on('submit', function(e){
-                if ($('span.error-info').length){
-                    console.log('エラーあり');
-                    e.preventDefault();
-                }
-            });
-        });
-    </script>
 </x-app-layout>
--- backend/resources/views/memos/show.blade.php
+++ backend/resources/views/memos/show.blade.php
@@ -20,9 +20,7 @@
                             <p class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="img" id="img">{{ $memo->img }}</p>
                         </div>
                     </div>
-                    <div class="col-6 col-md-4 col-xl-3 col-xs-12 mx-auto mt-5">
-                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-gray-700 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button"> 編集する</button>
-                    </div>
+                    <x-linkbutton href="{{ route('memos.edit', ['memo' => $memo->id]) }}"/>
                 </div>
             </div>
         </div>
