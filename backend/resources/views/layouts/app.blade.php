a399d901eef6dbe1a5efd27087e4d2169277aabd
kawatakutaku <kawataku.89love@gmail.com>
2022-02-06 20:37:38 +0900

    CU-25dpq8w linkを追加した

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
             @include('layouts.navigation')
 
             <!-- Page Heading -->
+            {{-- TODO: 色が反映されない --}}
             <header class="bg-gray-700 shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     {{ $header }}
