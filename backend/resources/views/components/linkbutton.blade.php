8b607c3e063577134551584094b72078a9a99127
kawatakutaku <kawataku.89love@gmail.com>
2022-02-06 20:35:14 +0900

    CU-25dpq8w componentの記述を作成した

--- backend/resources/views/components/linkbutton.blade.php
+++ backend/resources/views/components/linkbutton.blade.php
@@ -1,3 +1,3 @@
-<div class="col-6 col-md-4 col-xl-3 col-xs-12 mx-auto mt-5">
-    <a type="button" {{ $attributes->merge(["class" => "bg-blue-500 hover:bg-blue-700 text-gray-700 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"]) }}>編集する</a>
-</div>
+<a type="button" {{ $attributes->merge(["class" => "text-blue-300 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"]) }}>
+    {{ $slot }}
+</a>
