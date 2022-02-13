3ae1ce71eb0e28751e56f81740c896ba94fd8261
kawatakutaku <kawataku.89love@gmail.com>
2022-02-13 19:34:47 +0900

    CU-25dpqck todoを追加した

--- backend/resources/views/memos/index.blade.php
+++ backend/resources/views/memos/index.blade.php
@@ -10,21 +10,25 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    {{-- TODO: memos系のurlのときにnavにcreateも表示されるようにしたい --}}
+                    <x-linkbutton href="{{ route('memos.create') }}">
+                        <i class="fas fa-lg fa-pen"></i>
+                    </x-linkbutton>
                     @foreach($memos as $memo)
-                    <!-- TODO: どのユーザーの投稿かもわかるようにしたい-->
-                    <div class="row p-2 justify-content-center">
-                        <div class="col-6">
-                            <p class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="memo" id="memo">{{ $memo->memo }}</p>
-                            <p class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="img" id="img">{{ $memo->img }}</p>
+                        <!-- TODO: どのユーザーの投稿かもわかるようにしたい-->
+                        <div class="row p-2 justify-content-center">
+                            <div class="col-6">
+                                <p class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="memo" id="memo">{{ $memo->memo }}</p>
+                                <p class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="img" id="img">{{ $memo->img }}</p>
+                            </div>
+                            {{-- TODO: 詳細ボタンと削除ボタンを横並びにさせたい --}}
+                            <x-linkbutton href="{{ route('memos.show', ['memo' => $memo->id]) }}">
+                                <i class="fas fa-lg fa-file-alt"></i>
+                            </x-linkbutton>
+                            <form action="{{ route('memos.destroy', ['memo' => $memo->id]) }}" method="post">
+                                <x-delete />
+                            </form>
                         </div>
-                        <x-linkbutton href="{{ route('memos.create') }}">
-                            <i class="fas fa-lg fa-pen"></i>
-                        </x-linkbutton>
-                        <x-linkbutton href="{{ route('memos.show', ['memo' => $memo->id]) }}">
-                            <i class="fas fa-lg fa-file-alt"></i>
-                        </x-linkbutton>
-                        <i class="fas fa-lg fa-trash-alt"></i>
-                    </div>
                     @endforeach
                 </div>
             </div>
