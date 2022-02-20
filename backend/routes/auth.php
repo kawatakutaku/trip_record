789ace71a60d821c3a42036251a024d6ecddb155
kawatakutaku <kawataku.89love@gmail.com>
2022-02-20 10:10:01 +0900

    CU-2a2hway registerとloginのpost処理に対して、名前付きルートを作成した

--- backend/routes/auth.php
+++ backend/routes/auth.php
@@ -15,14 +15,16 @@ Route::get('/register', [RegisteredUserController::class, 'create'])
                 ->name('register');
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
-                ->middleware('guest');
+                ->middleware('guest')
+                ->name('register.post');
 
 Route::get('/login', [AuthenticatedSessionController::class, 'create'])
                 ->middleware('guest')
                 ->name('login');
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-                ->middleware('guest');
+                ->middleware('guest')
+                ->name('login.post');
 
 Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])
                 ->middleware('guest')
