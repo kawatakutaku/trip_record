bcd93c7bd54d2b05cd0483802558f10a90e75c69
kawatakutaku <kawataku.89love@gmail.com>
2022-02-20 16:23:37 +0900

    CU-2a2hway 新規登録のシナリオテストを作成した
    エラーが起きる部分はTODOを残している

--- backend/tests/Feature/Auth/RegistrationTest.php
+++ backend/tests/Feature/Auth/RegistrationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,22 +11,41 @@ use Tests\TestCase;
 
 class RegistrationTest extends BaseFeatureTestCase
 {
-    public function testRegisterForm()
+    protected $needLogin = false;
+
+    /**
+     * 新規登録画面
+     * @return void
+     */
+    public function testRegisterForm(): void
     {
         $response = $this->get('/register');
         $response->assertStatus(Response::HTTP_OK);
     }
 
-    public function testRegister()
+    /**
+     * 新規登録処理
+     * @return void
+     */
+    public function testRegister(): void
     {
-        $response = $this->post('/register', [
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'password',
-            'password_confirmation' => 'password',
-        ]);
+        $userData = $this->getUserData();
+
+        // TODO: controllerとview側も変更してDBに合わせる必要がある
+        $response = $this->post('/register', $userData);
 
         $this->assertAuthenticated();
         $response->assertRedirect(RouteServiceProvider::HOME);
+    }
+
+    /**
+     * 新規登録用のデータ
+     * @return array
+     */
+    public function getUserData(): array
+    {
+        $userData = User::factory()->raw();
+
+        return $userData;
     }
 }
