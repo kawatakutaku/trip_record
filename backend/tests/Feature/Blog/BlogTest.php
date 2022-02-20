0eda10a30b192e04d31b941cd55f064544a4d33e
kawatakutaku <kawataku.89love@gmail.com>
2022-02-20 15:45:14 +0900

    CU-2a2hway blogのシナリオテストを作成した

--- backend/tests/Feature/Blog/BlogTest.php
+++ backend/tests/Feature/Blog/BlogTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Blog;
 
 use App\Models\Blog;
+use App\Models\City;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,73 +16,139 @@ class BlogTest extends BaseFeatureTestCase
     {
         parent::setUp();
 
-        $this->blogId = Blog::inRandomOrder()->first()->id;
+        $this->cityId = City::inRandomOrder()->first()->id;
+    }
+
+    /**
+     * ブログのシナリオテスト
+     * @test
+     * @group blog
+     * @return void
+     */
+    public function testBlog(): void
+    {
+        $this->blogIndex();
+        $this->blogCreate();
+        $this->blogStore();
+        $this->blogShow();
+        $this->blogEdit();
+        $this->blogUpdate();
+        $this->blogDestroy();
     }
 
     /**
      * 一覧画面
+     * @test
+     * @group blog
      * @return void
      */
-    public function testBlogIndex()
+    private function blogIndex(): void
     {
-        $blogIndexResponse = $this->get(route("blogs.index"));
+        $blogIndexResponse = $this->get(route("blogs.index", [City::CITY_ID_NAME => $this->cityId]));
         $blogIndexResponse->assertStatus(Response::HTTP_OK);
     }
 
     /**
      * 新規作成画面
+     * @test
+     * @group blog
+     * @return void
      */
-    public function testBlogCreate()
+    private function blogCreate(): void
     {
-        $blogCreateResponse = $this->get(route("blogs.create"));
+        $blogCreateResponse = $this->get(route("blogs.create", [City::CITY_ID_NAME => $this->cityId]));
         $blogCreateResponse->assertStatus(Response::HTTP_OK);
     }
 
     /**
      * 保存処理
+     * @test
+     * @group blog
+     * @return void
      */
-    // public function testBlogStore()
-    // {
-    //     $blogStoreResponse = $this->post(route("blogs.store"));
-    //     $blogStoreResponse->assertStatus(Response::HTTP_FOUND);
-    //     $blogStoreResponse->assertRedirect(route("blogs.index"));
-    // }
+    private function blogStore(): void
+    {
+        $blogStoreData = $this->getBlogData();
+
+        // 保存処理のテスト
+        $blogStoreResponse = $this->post(route("blogs.store", [City::CITY_ID_NAME => $this->cityId]), $blogStoreData);
+        $blogStoreResponse->assertStatus(Response::HTTP_FOUND);
+        $blogStoreResponse->assertRedirect(route("blogs.index", [City::CITY_ID_NAME => $this->cityId]));
+
+        // 整合性チェック
+        $this->blogStored = Blog::orderBy("created_at", "desc")->first();
+        $this->assertEquals($this->blogStored->message, $blogStoreData[Blog::BLOG_MESSAGE]);
+    }
 
     /**
      * 詳細画面
+     * @test
+     * @group blog
+     * @return void
      */
-    public function testBlogShow()
+    private function blogShow(): void
     {
-        $blogShowResponse = $this->get(route("blogs.show", ["blog" => $this->blogId]));
+        $blogShowResponse = $this->get(route("blogs.show", [City::CITY_ID_NAME => $this->cityId, Blog::BLOG_ID_NAME => $this->blogStored->id]));
         $blogShowResponse->assertStatus(Response::HTTP_OK);
     }
 
     /**
      * 編集画面
+     * @test
+     * @group blog
+     * @return void
      */
-    public function testBlogEdit()
+    private function blogEdit(): void
     {
-        $blogEditResponse = $this->get(route("blogs.edit", ["blog" => $this->blogId]));
+        $blogEditResponse = $this->get(route("blogs.edit", [City::CITY_ID_NAME => $this->cityId, Blog::BLOG_ID_NAME => $this->blogStored->id]));
         $blogEditResponse->assertStatus(Response::HTTP_OK);
     }
 
     /**
      * 更新処理
+     * @test
+     * @group blog
+     * @return void
      */
-    // public function testBlogUpdate()
-    // {
-    //     $blogUpdateResponse = $this->patch(route("blogs.update", ["blog" => $this->blogId]));
-    //     $blogUpdateResponse->assertStatus(Response::HTTP_FOUND);
-    //     $blogUpdateResponse->assertRedirect(route("blogs.show", ["blog" => $this->blogId]));
-    // }
+    private function blogUpdate(): void
+    {
+        $blogUpdateData = $this->getBlogData();
+
+        // 更新処理のテスト
+        $blogUpdateResponse = $this->patch(route("blogs.update", [City::CITY_ID_NAME => $this->cityId, Blog::BLOG_ID_NAME => $this->blogStored->id]), $blogUpdateData);
+        $blogUpdateResponse->assertStatus(Response::HTTP_FOUND);
+        $blogUpdateResponse->assertRedirect(route("blogs.show", [City::CITY_ID_NAME => $this->cityId, Blog::BLOG_ID_NAME => $this->blogStored->id]));
+
+        // 整合性チェック
+        $blogUpdated = Blog::find($this->blogStored->id);
+        $this->assertEquals($blogUpdated->message, $blogUpdateData[Blog::BLOG_MESSAGE]);
+    }
 
     /**
      * 削除処理
+     * @test
+     * @group blog
+     * @return void
      */
-    public function testBlogDestroy()
+    private function blogDestroy(): void
     {
-        $blogDestroyResponse = $this->delete(route("blogs.destroy", ["blog" => $this->blogId]));
+        $blogDestroyResponse = $this->delete(route("blogs.destroy", [City::CITY_ID_NAME => $this->cityId, Blog::BLOG_ID_NAME => $this->blogStored->id]));
         $blogDestroyResponse->assertStatus(Response::HTTP_FOUND);
-        $blogDestroyResponse->assertRedirect(route("blogs.index"));
+        $blogDestroyResponse->assertRedirect(route("blogs.index", [City::CITY_ID_NAME => $this->cityId]));
+
+        // 整合性チェック
+        $blogDestroyed = Blog::find($this->blogStored->id);
+        $this->assertNull($blogDestroyed);
+    }
+
+    /**
+     * ブログのデータ
+     * @return array
+     */
+    public function getBlogData(): array
+    {
+        $blogData = Blog::factory()->raw();
+
+        return $blogData;
     }
 }
