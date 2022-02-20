444529fb34ac2217920c6ebd7c714ade0d2d26b6
kawatakutaku <kawataku.89love@gmail.com>
2022-02-20 11:36:14 +0900

    CU-2a2hway メモのシナリオテストを作成した

--- backend/tests/Feature/Memo/MemoTest.php
+++ backend/tests/Feature/Memo/MemoTest.php
@@ -8,109 +8,136 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Symfony\Component\HttpFoundation\Response;
+use Tests\Feature\BaseFeatureTestCase;
 use Tests\TestCase;
 
-class MemoTest extends TestCase
+class MemoTest extends BaseFeatureTestCase
 {
 
     protected function setUp(): void
     {
         parent::setUp();
 
-        // TODO: ログインの処理を記述できていないため、暫定的にDBから取得するようにする
-        $this->userId = User::inRandomOrder()->first()->id;
         $this->cityId = City::inRandomOrder()->first()->id;
-        // TODO: storeで使用したmemoのidを使うようにしたい
-        $this->memoId = Memo::inRandomOrder()->first()->id;
     }
+
+    /**
+     * メモのシナリオテスト
+     * @test
+     * @group memo
+     * @return void
+     */
+    public function testMemo(): void
+    {
+        $this->memoIndex();
+        $this->memoCreate();
+        $this->memoStore();
+        $this->memoShow();
+        $this->memoEdit();
+        $this->memoUpdate();
+        $this->memoDestroy();
+    }
+
 
     /**
      * 一覧画面
      * @return void
      */
-    public function testMemoIndex()
+    private function memoIndex(): void
     {
-        $memoIndexResponse = $this->get(route("memos.index"));
+        $memoIndexResponse = $this->get(route("memos.index", [City::CITY_ID_NAME => $this->cityId]));
         $memoIndexResponse->assertStatus(Response::HTTP_OK);
     }
     /**
      * メモの新規作成画面
+     * @return void
      */
-    public function testMemoCreate()
+    private function memoCreate(): void
     {
-        $memoCreateResponse = $this->get(route("memos.create"));
+        $memoCreateResponse = $this->get(route("memos.create", [City::CITY_ID_NAME => $this->cityId]));
         $memoCreateResponse->assertStatus(Response::HTTP_OK);
     }
     /**
      * メモの保存処理
+     * @return void
      */
-    // TODO: cityIdが渡せず500エラーが出るから、コメントアウトしておく
-    // public function testMemoStore()
-    // {
-    //     // 保存用のデータ
-    //     $memoStoreData = $this->getMemoData();
+    private function memoStore(): void
+    {
+        // 保存用のデータ
+        $memoStoreData = $this->getMemoData();
 
-    //     // 保存処理のテスト
-    //     $memoStoreResponse = $this->post(route("memos.store"), $memoStoreData);
-    //     $memoStoreResponse->assertStatus(Response::HTTP_FOUND);
-    //     $memoStoreResponse->assertRedirect(route("memos.index"));
+        // 保存処理のテスト
+        $memoStoreResponse = $this->post(route("memos.store", [City::CITY_ID_NAME => $this->cityId]), $memoStoreData);
+        $memoStoreResponse->assertStatus(Response::HTTP_FOUND);
+        $memoStoreResponse->assertRedirect(route("memos.index", [City::CITY_ID_NAME => $this->cityId]));
 
-    //     // 整合性チェック
-    //     $this->memoRecord = Memo::where("user_id", $memoStoreData['user_id'])->orderBy("created_at", "desc")->first();
-    //     // TODO: 基底クラスに整合性チェック用のfor文を書いて、チェックしたい
-    //     $this->assertEquals($this->memoRecord->memo, $memoStoreData['memo']);
-    //     $this->assertEquals($this->memoRecord->img, $memoStoreData['img']);
-    // }
+        // 整合性チェック
+        // TODO: storeで保存したデータを厳密には取得できてない可能性がある
+        $this->memoStored = Memo::orderBy("created_at", "desc")->first();
+
+        // TODO: 基底クラスに整合性チェック用のfor文を書いて、チェックしたい
+        $this->assertEquals($this->memoStored->memo, $memoStoreData[Memo::MEMO_MEMO]);
+        $this->assertEquals($this->memoStored->img, $memoStoreData[Memo::MEMO_IMG]);
+    }
     /**
      * メモの詳細画面
+     * @return void
      */
-    public function testMemoShow()
+    private function memoShow(): void
     {
-        $memoShowResponse = $this->get(route("memos.show", ["memo" => $this->memoId]));
+        $memoShowResponse = $this->get(route("memos.show", [City::CITY_ID_NAME => $this->cityId, Memo::MEMO_ID_NAME => $this->memoStored->id]));
         $memoShowResponse->assertStatus(Response::HTTP_OK);
     }
     /**
      * メモの編集画面
+     * @return void
      */
-    public function testMemoEdit()
+    private function memoEdit(): void
     {
-        $memoEditResponse = $this->get(route("memos.edit", ["memo" => $this->memoId]));
+        $memoEditResponse = $this->get(route("memos.edit", [City::CITY_ID_NAME => $this->cityId, Memo::MEMO_ID_NAME => $this->memoStored->id]));
         $memoEditResponse->assertStatus(Response::HTTP_OK);
     }
     /**
      * メモの更新処理
+     * @return void
      */
-    // TODO: cityIdが渡せず500エラーが出るから、コメントアウトしておく
-    // public function testMemoUpdate()
-    // {
-    //     $memoUpdateResponse = $this->patch(route("memos.update", ["memo" => $this->memoId]));
-    //     $memoUpdateResponse->assertStatus(Response::HTTP_FOUND);
-    //     $memoUpdateResponse->assertRedirect(route("memos.show", ["memo" => $this->memoId]));
+    private function memoUpdate(): void
+    {
+        // 更新用のデータ
+        $memoUpdateData = $this->getMemoData();
 
-    //     // TODO: 整合性チェックもやりたい
-    // }
+        $memoUpdateResponse = $this->patch(route("memos.update", [City::CITY_ID_NAME => $this->cityId, Memo::MEMO_ID_NAME => $this->memoStored->id]), $memoUpdateData);
+        $memoUpdateResponse->assertStatus(Response::HTTP_FOUND);
+        $memoUpdateResponse->assertRedirect(route("memos.show", [City::CITY_ID_NAME => $this->cityId, Memo::MEMO_ID_NAME => $this->memoStored->id]));
+
+        // TODO: 整合性チェックを基底クラスの方にfor文を書いて行いたい
+        $this->memoUpdated = Memo::find($this->memoStored->id);
+        $this->assertEquals($this->memoUpdated->memo, $memoUpdateData[Memo::MEMO_MEMO]);
+        $this->assertEquals($this->memoUpdated->img, $memoUpdateData[Memo::MEMO_IMG]);
+    }
     /**
      * メモの削除処理
+     * @return void
      */
 
-    public function testMemoDestroy()
+    private function memoDestroy(): void
     {
-        $memoDestroyResponse = $this->delete(route("memos.destroy", ["memo" => $this->memoId]));
+        $memoDestroyResponse = $this->delete(route("memos.destroy", [City::CITY_ID_NAME => $this->cityId, Memo::MEMO_ID_NAME => $this->memoStored->id]));
         $memoDestroyResponse->assertStatus(Response::HTTP_FOUND);
-        $memoDestroyResponse->assertRedirect(route("memos.index"));
+        $memoDestroyResponse->assertRedirect(route("memos.index", [City::CITY_ID_NAME => $this->cityId]));
 
-        // TODO: 整合性チェックもやりたい
+        // 整合性チェック
+        $this->memoDestroyed = Memo::find($this->memoStored->id);
+        $this->assertNull($this->memoDestroyed);
     }
 
     /**
      * post用のデータ
+     * @return array
      */
-    public function getMemoData()
+    public function getMemoData(): array
     {
-        $memoData = Memo::factory()->raw([
-            'user_id' => $this->userId,
-            'city_id' => $this->cityId,
-        ]);
+        $memoData = Memo::factory()->raw();
 
         return $memoData;
     }
