62c6faebf1819217351c879860905e0082b74888
kawatakutaku <kawataku.89love@gmail.com>
2022-03-19 19:04:16 +0900

    CU-25dpqd4 メモの返信のindexのみ記述した

--- backend/app/Http/Controllers/Memos/MemoResponseController.php
+++ backend/app/Http/Controllers/Memos/MemoResponseController.php
@@ -5,28 +5,34 @@ namespace App\Http\Controllers\Memos;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreMemoResponseRequest;
 use App\Http\Requests\UpdateMemoResponseRequest;
+use App\Models\Memo;
 use App\Models\MemoResponse;
+use App\Models\User;
+use Illuminate\View\View;
 
 class MemoResponseController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @param string $memoId
+     * @return \Illuminate\View\View
      */
-    public function index()
+    public function index(string $memoId): View
     {
-        //
+        $user = app(User::class);
+        $userId = $user->getAuthAccountId();
+
+        $memoResponse = MemoResponse::where('memo_id', $memoId)->where('user_id', $userId)->get();
+
+        return view('memoResponse.index', [MemoResponse::MEMO_ID => $memoId, MemoResponse::MEMO_RESPONSES => $memoResponse]);
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @param string $memoId
+     * @return \Illuminate\View\View
      */
-    public function create()
+    public function create(string $memoId): View
     {
-        //
+        return view('memoResponse.create', [MemoResponse::MEMO_ID => $memoId]);
     }
 
     /**
